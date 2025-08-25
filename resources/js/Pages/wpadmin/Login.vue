1ab32828a7ad35172dd1df9653e7484d69958c16
kickymaulana <kickymaulana@gmail.com>
2025-08-25 14:24:53 +0700

    menampilkan halaman login

--- resources/js/Pages/wpadmin/Login.vue
+++ resources/js/Pages/wpadmin/Login.vue
@@ -1,5 +1,64 @@
 <template>
-    <div>
-        <h1>Login</h1>
-    </div>
+    <main>
+        <div class="container">
+
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="#" class="logo d-flex align-items-center w-auto">
+                                    <img src="../../../../public/NiceAdmin/assets/img/logo.png" alt="">
+                                    <span class="d-none d-lg-block">Website OPD 2025</span>
+                                </a>
+                            </div><!-- End Logo -->
+
+                            <div class="card mb-3">
+
+                                <div class="card-body">
+
+                                    <div class="pt-4 pb-2">
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
+                                    </div>
+
+                                    <form class="row g-3" wire:submit.prevent="login" novalidate>
+
+                                        <div class="col-12">
+                                            <label for="yourUsername" class="form-label">Username</label>
+                                            <div class="input-group">
+                                                <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="yourUsername" required wire:model="username">
+                                                <div class="invalid-feedback">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label for="yourPassword" class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="yourPassword" required wire:model="password">
+                                            <div class="invalid-feedback">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100 mt-3" type="submit">Login</button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+        </div>
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 </template>
