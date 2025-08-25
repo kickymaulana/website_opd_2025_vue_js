f95df742ffec61ecfdd577f4be6f305f7718795e
kickymaulana <kickymaulana@gmail.com>
2025-08-25 07:58:54 +0700

    membuat route login dan login controller

---
+++ app/Http/Controllers/wpadmin/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\wpadmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('HelloWorld');
 });
+
+Route::get('wp-admin/login')->name('login');
