c58ca52598a7b2f680f7ecf7585cfcc2f9307496
kickymaulana <kickymaulana@gmail.com>
2025-08-25 08:08:52 +0700

    membuat function login

--- app/Http/Controllers/wpadmin/LoginController.php
+++ app/Http/Controllers/wpadmin/LoginController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\wpadmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class LoginController extends Controller
 {
-    //
+    public function login()
+    {
+        return Inertia::render('wpadmin/Login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\wpadmin\LoginController;
 
 Route::get('/', function () {
     return Inertia::render('HelloWorld');
 });
 
-Route::get('wp-admin/login')->name('login');
+Route::get('wp-admin/login', [LoginController::class, 'login'])->name('login');
